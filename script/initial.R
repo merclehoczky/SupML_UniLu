c7495b4f85a43e69d7c9e33437fe8405267c312f
Mercédesz Lehoczky <merc.lehoczky@gmail.com>
2023-11-09 15:56:55 +0100

    Update setwd and paths

--- script/initial.R
+++ script/initial.R
@@ -1,9 +1,16 @@
 library(readr)
 library(dplyr)
 
+library(rstudioapi)
+
+# Getting the path of your current open file
+current_path = rstudioapi::getActiveDocumentContext()$path 
+setwd(dirname(current_path ))
+print( getwd() )
+
 # Upload data
-path_train <- "/Users/viktoriiatantsiura/Downloads/University_Luzern/3 Semester/Machine learning/Final Project/training_data.csv"
-path_test <- "/Users/viktoriiatantsiura/Downloads/University_Luzern/3 Semester/Machine learning/Final Project/X_test.csv"
+path_train <- "../data/training_data.csv"
+path_test <- "../data/X_test.csv"
 training_data <- read_csv(path_train)
 x_test <- read_csv(path_test)
 
