7bf04df4e423633deea16dfe1b6a50e6b786f50d
Mercédesz Lehoczky <merc.lehoczky@gmail.com>
2023-11-25 01:14:08 +0100

    master code for running API

---
+++ script/2_master_distances.R
@@ -0,0 +1,24 @@
+# Master Script
+
+# Add data to temporary variable --------------------------
+data <-  training_data
+
+# Run distances scripts with Google API
+# Run script to set up Google API usage and functions
+source("distance.R")
+
+# Run script that calculates distance to nearest lake
+source("dist_lake.R")
+
+# Run script that calculates distance to nearest primary school
+source("dist_school.R")
+
+# Run script that calculates distance to nearest public transport stop
+source("nearpublic.R")
+
+# Run script that calculates distance to nearest train station
+source("totrain.R")
+
+# Save dataset ------------------
+write.csv(data, "../data/after_api.csv", row.names = FALSE)
+
