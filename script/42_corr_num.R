0f4234de5231c6a567ede29907274c9228648d39
Mercédesz Lehoczky <merc.lehoczky@gmail.com>
2023-11-26 22:56:29 +0100

    renamed corr_num, corr_cat will be in new script

--- script/42_corr_num.R
+++ script/42_corr_num.R
@@ -11,14 +11,14 @@ summary(df %>% select_if(is.numeric))
 
 # Numerical relevant columns
 vars_num_names <- names(df)[sapply(df, is.numeric)]
-# 
+df_no_na <- na.omit(df[vars_num_names])
 # numer <- c("rent_full", "Micro_rating", "Micro_rating_NoiseAndEmission", 
 #            "Micro_rating_Accessibility", "Micro_rating_DistrictAndArea", 
 #            "Micro_rating_SunAndView", "Micro_rating_ServicesAndNature", "Noise_max", "apoth_pix_count_km2", 
 #            "dist_to_4G", "dist_to_5G", "dist_to_train_stat", "restaur_pix_count_km2", 
 #            "superm_pix_count_km2", "dist_to_river")
 
-cor_matrix <- cor(df[vars_num_names], use = "complete.obs")
+cor_matrix <- cor(df_no_na[vars_num_names], use = "complete.obs")
 corrplot(cor_matrix, method = "circle")
 
 # Set the size of the plotting device
@@ -66,29 +66,3 @@ variables_to_remove <- unique(c( high_corr_vars$Variable2))
 # Remove the variables from the data frame
 df_filtered <- df[, !names(df) %in% variables_to_remove]
 
-
-
-# Categorical variables correlation --------------------------------------------
-# Save binary variables
-
-vars_binary_names <- names(df)[sapply(df, function(x) is.factor(x) && length(levels(x)) == 2)]
-vars_cat_names <- names(df)[sapply(df, is.factor) | sapply(df, is.logical) | (names(df) %in% vars_binary_names)]
-
-# Set the size of the plotting device
-png("Correlation_plot_categorical.png", width = 800, height = 800)
-
-# Create a contingency table for the binary variables
-contingency_table <- table(df[, vars_cat_names, drop = FALSE])
-
-# Perform chi-squared test
-chi2_result <- chisq.test(contingency_table)
-
-# Print the chi-squared test result
-print(chi2_result)
-
-# Plot the chi-squared correlation matrix
-corrplot(chi2_result$stdres, method = "circle", type = "upper", order = "hclust", tl.pos = "lt", tl.col = "black")
-
-# Close the plotting device
-
-dev.off()
