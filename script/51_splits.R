71b88470d97ad9defc7931bb030295fb2c9f8256
Mercédesz Lehoczky <merc.lehoczky@gmail.com>
2023-11-29 21:04:34 +0100

    changed recipe

--- script/51_splits.R
+++ script/51_splits.R
@@ -28,8 +28,8 @@ data_recipe <- recipe(rent_full ~ ., data = train_data) %>%
   step_naomit(all_predictors()) %>% 
   step_zv(all_predictors()) %>%
   step_dummy(all_nominal(), one_hot = TRUE) %>%  
-  step_normalize(all_numeric()) %>%
-  step_scale(all_numeric()) %>% 
+  step_normalize(all_numeric(), -all_outcomes()) %>%
+  step_scale(all_numeric(), -all_outcomes()) %>% 
   prep()
   
 
