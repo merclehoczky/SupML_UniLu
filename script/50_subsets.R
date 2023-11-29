37b03c91390e580ff500dffca46f32c9f4892cfa
Mercédesz Lehoczky <merc.lehoczky@gmail.com>
2023-11-29 22:48:15 +0100

    remove noise mecause it was correlated

--- script/50_subsets.R
+++ script/50_subsets.R
@@ -1,18 +1,18 @@
 # subset 1 ----------------------------------------------
 # without micro_rating, and selected vars included in microratings
 
-df_wo_svars <- df %>% 
-  select(- c(Micro_rating, Noise_max, wgh_avg_sonnenklasse_per_egid, 
+df_wo_svars <- df_corr %>% 
+  select(- c(Micro_rating, wgh_avg_sonnenklasse_per_egid, 
          dist_to_haltst, dist_to_highway, dist_to_main_stat, dist_to_train_stat,
          dist_to_lake, dist_to_river))
 
 # subset 2 ----------------------------------------------------------
 # without all micro_rating except main one
 
-df_wo_mrs <- df %>% 
+df_wo_mrs <- df_corr %>% 
   select(- c(Micro_rating_NoiseAndEmission, Micro_rating_Accessibility, Micro_rating_DistrictAndArea,
          Micro_rating_SunAndView, Micro_rating_ServicesAndNature))
 
 
-df_wo_mrs_others <- df %>% 
+df_wo_mrs_others <- df_wo_mrs %>% 
   select(-c(cabletv, dryer, oven ))
