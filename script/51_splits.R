8b72d628f35f9b96de0c9871f64b19edb1803158
Mercédesz Lehoczky <merc.lehoczky@gmail.com>
2023-11-30 01:33:51 +0100

    reorganised recipe

--- script/51_splits.R
+++ script/51_splits.R
@@ -29,9 +29,9 @@ train_folds <- vfold_cv(data = data_fin, v = 4)
 data_recipe <- recipe(rent_full ~ ., data = train_data) %>%
   step_naomit(all_predictors()) %>% 
   step_zv(all_predictors()) %>%
-  step_dummy(all_nominal(), one_hot = TRUE) %>%  
   step_normalize(all_numeric(), -all_outcomes()) %>%
   step_scale(all_numeric(), -all_outcomes()) %>% 
+  step_dummy(all_nominal(), one_hot = TRUE) %>%  
   prep()
   
 
