ef515864dbfe68945d2e5d1ddd1fe4fa8fb2551f
Viktoriia Tantsiura <93945148+ViktoriiaTan@users.noreply.github.com>
2023-11-30 21:47:58 +0100

    added new option for subset

--- script/51_splits.R
+++ script/51_splits.R
@@ -15,6 +15,8 @@ data_fin <- df_corr
 data_fin <- df_wo_mrs  #using this
 #data_fin <- df_wo_mrs_others
 #data_fin <- df_wo_variability
+#data_fin <- df_new
+
 # Create a 80-20 train-test split
 
 df_split <- initial_split(data_fin, prop = 0.8)
